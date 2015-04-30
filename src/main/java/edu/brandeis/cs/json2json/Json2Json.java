e9870d2e0576eb81d8100046824a9281b032f14b
Chunqi Shi <chqshi@gmail.com>
2015-04-30 02:52:41 +0100

    json2josn

--- src/main/java/edu/brandeis/cs/json2json/Json2Json.java
+++ src/main/java/edu/brandeis/cs/json2json/Json2Json.java
@@ -183,7 +183,9 @@ public static void json2node(JsonProxy.JsonObject jsonObj, XMLStreamWriter xmlSt
     public static JsonProxy.JsonObject node2json(Node node, JsonProxy.JsonObject jsonObj) {
         if(node.getNodeType() == Node.ELEMENT_NODE
                 || node.getNodeType() == Node.DOCUMENT_NODE
-                || node.getNodeType() == Node.COMMENT_NODE) {
+                || node.getNodeType() == Node.COMMENT_NODE
+                || node.getNodeType() == Node.TEXT_NODE
+                || node.getNodeType() == Node.PROCESSING_INSTRUCTION_NODE) {
             // node attributes
             NamedNodeMap attrs = node.getAttributes();
             if(attrs != null) {
@@ -209,6 +211,8 @@ public static void json2node(JsonProxy.JsonObject jsonObj, XMLStreamWriter xmlSt
                             jsonObj.put("#comment", comms);
                         }
                     }
+                }else if(list.item(i).getNodeType() == Node.PROCESSING_INSTRUCTION_NODE) {
+                    commentNodes.add(list.item(i));
                 }
             }
             // remove all comment nodes
